57d6e04b8c6388a910817fafabf996298b3c0065
Wealthometer <wealthhajoh87@gmail.com>
2025-10-03 22:19:17 +0100

    Update splash_screen.dart

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -124,7 +124,44 @@ class SplashScreen extends StatelessWidget {
 
                 ],
               ),
+            ),
+
+
+            // bottom tagline
+            Positioned(
+              bottom: 48,
+              left: 0,
+              right: 0,
+              child: TweenAnimationBuilder<double>(
+                tween: Tween(begin: 0.0, end: 1.0),
+                duration: const Duration(milliseconds: 1200),
+                builder: (context, value, child){
+                  return Opacity(
+                      opacity: value,
+                      child: child
+                  );
+                },
+
+                child: Text(
+                  "Style Meets Simplicity ¡!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 14,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.w300,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                          color: Colors.black54,
+                        )
+                      ],
+                  ),
+                ),
+              ),
             )
+
           ],
         ),
       )
