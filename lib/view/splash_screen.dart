ad671dcb4089655e9784baa314312ded5f82e8c9
Wealthometer <wealthhajoh87@gmail.com>
2025-10-03 02:20:41 +0100

    update splash_screen.dart

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -23,7 +23,9 @@ class SplashScreen extends StatelessWidget {
             Positioned.fill(
                 child: Opacity(
                   opacity: 0.05,
-                  child: GridPattern(),
+                  child: GridPattern(
+                    color: Colors.white
+                  ),
                 )
             )
           ],
@@ -34,10 +36,43 @@ class SplashScreen extends StatelessWidget {
 }
 
 class GridPattern extends StatelessWidget {
-  const GridPattern({super.key});
+  final Color color;
+
+  const GridPattern ({
+    Key? key,
+    required this.color
+  }): super(key : key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomPaint(
+      painter: GridPainter(color: color),
+    );
   }
+}
+
+class GridPainter extends CustomPainter {
+  final Color color;
+
+  GridPainter({required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+        ..color = color
+        ..strokeWidth = 0.5;
+
+    final spacing = 20.0;
+
+    for( var i = 0.0; i < size.width; i += spacing ) {
+      canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint);
+    }
+
+    for( var i = 0.0; i < size.height; i += spacing ) {
+      canvas.drawLine(Offset(0, i), Offset(size.width, i), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDeletate) => false;
 }
