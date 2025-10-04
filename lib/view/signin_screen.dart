be76ba544b539ca58292e946a839c545f31d124d
Wealthometer <wealthhajoh87@gmail.com>
2025-10-05 00:56:46 +0100

    Create signin_screen.dart

---
+++ lib/view/signin_screen.dart
@@ -0,0 +1,48 @@
+import 'package:ecommerce_app_ui/utils/app_textstyles.dart';
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatelessWidget {
+  const SigninScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 41,
+                ),
+                Text(
+                  "Welcome Back!¡",
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.h1,
+                    Theme.of(context).textTheme.bodyLarge!.color!
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Sign In To Continue Shopping",
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.bodyLarge,
+                    isDark ? Colors.grey[400]! : Colors.grey[600]!
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
