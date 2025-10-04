6acea52ed314e87296f55668d8b535f1bd01e22c
Wealthometer <wealthhajoh87@gmail.com>
2025-10-04 15:47:29 +0100

    Update onboarding_screen.dart

--- lib/view/widgets/onboarding_screen.dart
+++ lib/view/widgets/onboarding_screen.dart
@@ -91,7 +91,63 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               right: 0,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                      _items.length,
+                      (index) => AnimatedContainer(
+                        duration: Duration(microseconds: 300),
+                        margin: EdgeInsets.symmetric(horizontal: 4),
+                        height: 8,
+                        width: _currentPage == index ? 24 : 8,
+                        decoration: BoxDecoration(
+                          color: _currentPage == index
+                              ? Theme.of(context).primaryColor
+                              : (isDark ? Colors.grey[700] : Colors.grey[300]),
+                          borderRadius: BorderRadius.circular(4)
+                        ),
+                      )
+                  ),
               ),
+          ),
+        Positioned(
+          bottom: 16,
+          left: 16,
+          right: 16,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                  onPressed: (){},
+                  child: Text(
+                    "Skip",
+                    style: AppTextStyle.withColor(
+                        AppTextStyle.buttonMedium,
+                        isDark ? Colors.grey[400]! : Colors.grey[600]!
+                    ),
+                  )
+              ),
+              ElevatedButton(
+                onPressed: (){
+                if(_currentPage < _items.length - 1) {
+                  _pageController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+                }else{}
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 16
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)
+                )
+              ),    
+              child: Text(
+                _currentPage < _items.length - 1 ? "Next" : "Get Started",
+                style: AppTextStyle.withColor(AppTextStyle.buttonMedium, Colors.white),
+              )
+              )
+            ],
+          )
           )
         ],
       ),
