506d69bd556072530fc42dc2140d38c3d5cf7237
Wealthometer <wealthhajoh87@gmail.com>
2025-10-05 01:24:45 +0100

    Update signin_screen.dart

--- lib/view/signin_screen.dart
+++ lib/view/signin_screen.dart
@@ -6,42 +6,35 @@ class SigninScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final isDark = Theme.of(context).brightness == Brightness.dark;
 
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 41,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 41),
+              Text(
+                "Welcome Back!¡",
+                style: AppTextStyle.withColor(
+                  AppTextStyle.h1,
+                  Theme.of(context).textTheme.bodyLarge!.color!,
                 ),
-                Text(
-                  "Welcome Back!¡",
-                  style: AppTextStyle.withColor(
-                    AppTextStyle.h1,
-                    Theme.of(context).textTheme.bodyLarge!.color!
-                  ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "Sign In To Continue Shopping",
+                style: AppTextStyle.withColor(
+                  AppTextStyle.bodyLarge,
+                  isDark ? Colors.grey[400]! : Colors.grey[600]!,
                 ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  "Sign In To Continue Shopping",
-                  style: AppTextStyle.withColor(
-                    AppTextStyle.bodyLarge,
-                    isDark ? Colors.grey[400]! : Colors.grey[600]!
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
-          )
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+        ),
       ),
     );
   }
