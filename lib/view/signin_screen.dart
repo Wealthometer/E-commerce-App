d170f615d28009e3ff678a66733caf7471eda97f
Wealthometer <wealthhajoh87@gmail.com>
2025-10-05 08:30:40 +0100

    Update signin_screen.dart

--- lib/view/signin_screen.dart
+++ lib/view/signin_screen.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app_ui/utils/app_textstyles.dart';
 import 'package:ecommerce_app_ui/view/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SigninScreen extends StatelessWidget {
   SigninScreen({super.key});
@@ -41,7 +42,76 @@ class SigninScreen extends StatelessWidget {
                 label: "Email",
                 prefixIcon: Icons.email_outlined,
                 keyboardType: TextInputType.emailAddress,
-                controller: ,
+                controller: _emailController,
+                validator: (value){
+                  if (value == null ||value.isEmpty) {
+                    return "Please enter your email";
+                  }
+                  if (!GetUtils.isEmail(value)) {
+                    return "Please enter a valid email";
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(
+                height: 16,
+              ),
+
+              CustomTextfield(
+                label: "Password",
+                prefixIcon: Icons.lock_outline_sharp,
+                keyboardType: TextInputType.visiblePassword,
+                isPassword: true,
+                controller: _passwordController,
+                validator: (value){
+                  if (value == null ||value.isEmpty) {
+                    return "Please enter your Password";
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(
+                height: 8,
+              ),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: (){},
+                    child: Text(
+                      "Forgot Password",
+                      style: AppTextStyle.withColor(
+                          AppTextStyle.buttonMedium,
+                          Theme.of(context).primaryColor
+                      ),
+                    )
+                ),
+              ),
+
+              const SizedBox(
+                height: 24,
+              ),
+              
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _handleSignIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text('Sign In',
+                    style: AppTextStyle.withColor(
+                      AppTextStyle.buttonMedium,
+                      Colors.white,
+                    ),
+                  ),
+                ),
               )
             ],
           ),
@@ -49,4 +119,10 @@ class SigninScreen extends StatelessWidget {
       ),
     );
   }
+
+  //Sign in btn onpressed
+
+void _handleSignIn() {
+    
+}
 }
