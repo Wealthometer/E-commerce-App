a6b774d4a7dad49c8459edf879be4e9946a5f156
Wealthometer <wealthhajoh87@gmail.com>
2025-11-06 00:31:06 +0100

    Update main_screen.dart

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,10 +1,16 @@
+import 'package:ecommerce_app_ui/controllers/navigation_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final NavigationController navigationController = Get.put(
+      NavigationController()
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
