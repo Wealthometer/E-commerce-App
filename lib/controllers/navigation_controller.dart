de71a7d27e9c197e82a383e928c51c3071c6662d
Wealthometer <wealthhajoh87@gmail.com>
2025-11-06 00:31:12 +0100

    Create navigation_controller.dart

---
+++ lib/controllers/navigation_controller.dart
@@ -0,0 +1,13 @@
+import 'package:ecommerce_app_ui/view/widgets/custom_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NavigationController {
+
+  final RxInt currentIndex = 0.obs;
+
+  void changeIndex(int index){
+    currentIndex.value = index;
+  }
+
+}
