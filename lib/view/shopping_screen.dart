0b911566f378d04a5c3640fdbf8c191707ec64ba
Wealthometer <wealthhajoh87@gmail.com>
2025-11-06 11:58:46 +0100

    Create shopping_screen.dart

---
+++ lib/view/shopping_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ShoppingScreen extends StatelessWidget {
+  const ShoppingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            'Shopping Screen'
+        ),
+      ),
+    );
+  }
+}
