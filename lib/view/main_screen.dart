1b15182c176b6cfc6fb8d8cec2a0a29a2cad01b3
Wealthometer <wealthhajoh87@gmail.com>
2025-11-06 11:58:29 +0100

    Update main_screen.dart

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,7 +1,15 @@
 import 'package:ecommerce_app_ui/controllers/navigation_controller.dart';
 import 'package:ecommerce_app_ui/controllers/theme_controller.dart';
+import 'package:ecommerce_app_ui/view/shopping_screen.dart';
+import 'package:ecommerce_app_ui/view/widgets/custom_bottom_navbar.dart';
+import 'package:ecommerce_app_ui/view/wishlist_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ecommerce_app_ui/view/account_screen.dart';
+import 'package:ecommerce_app_ui/view/home_screen.dart';
+// import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+// import 'package:get/get_state_manager/src/simple/get_state.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -24,14 +32,15 @@ class MainScreen extends StatelessWidget {
                     key: ValueKey(navigationController.currentIndex.value),
                     index: navigationController.currentIndex.value,
                     children: [
-                      // HomeScreen(),
-                      // ShoppingScreen(),
-                      // WishlistScreen(),
-                      // AccountScreen(),
+                      HomeScreen(),
+                      ShoppingScreen(),
+                      WishlistScreen(),
+                      AccountScreen(),
                     ],
                   )
               ),
           ),
+          bottomNavigationBar: CustomBottomNavbar(),
         )
     );
   }
