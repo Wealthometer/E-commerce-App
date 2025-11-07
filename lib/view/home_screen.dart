8df3e2203f3c94d58178180c9ba06d4b0c4a5e87
Wealthometer <wealthhajoh87@gmail.com>
2025-11-07 02:15:06 +0100

    Update home_screen.dart

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:ecommerce_app_ui/controllers/theme_controller.dart';
+import 'package:ecommerce_app_ui/view/widgets/custom_search_bar.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: const SafeArea(
+      body: SafeArea(
         child: Column(
           children: [
             Padding(
@@ -22,10 +25,65 @@ class HomeScreen extends StatelessWidget {
                     radius: 20,
                     backgroundImage: AssetImage('assets/images/avatar.jpg'),
                   ),
-                  
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello Wealth',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Good Day',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Spacer(),
+
+                  IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(
+                        Icons.notifications_outlined
+                      )
+                  ),
+
+                  IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(
+                          Icons.shopping_bag_outlined
+                      )
+                  ),
+
+                  GetBuilder<ThemeController>(
+                    builder: (controller) => IconButton(
+                      onPressed: () => controller.toggleTheme(),
+                      icon: Icon(
+                        controller.isDarkMode
+                            ? Icons.light_mode
+                            : Icons.dark_mode
+                      ),
+                    ),
+                  )
                 ],
               ),
-            )
+            ),
+
+            const CustomSearchBar()
           ],
         ),
       ),
