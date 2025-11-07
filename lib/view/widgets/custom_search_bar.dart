c615859bb110f65781091315a2d1ef46b2040e4b
Wealthometer <wealthhajoh87@gmail.com>
2025-11-07 09:53:50 +0100

    Update custom_search_bar.dart

--- lib/view/widgets/custom_search_bar.dart
+++ lib/view/widgets/custom_search_bar.dart
@@ -4,37 +4,62 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class CustomSearchBar extends StatelessWidget {
-  const CustomSearchBar ({super.key});
+  const CustomSearchBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
 
     return Padding(
-      padding: EdgeInsets.all(
-          10
-      ),
+      padding: EdgeInsets.all(10),
       child: TextField(
         style: AppTextStyle.withColor(
           AppTextStyle.buttonMedium,
-          Theme.of(context).textTheme.bodyLarge?.color ?? Colors.black
+          Theme.of(context).textTheme.bodyLarge?.color ?? Colors.black,
         ),
+
         decoration: InputDecoration(
+
           hintText: 'Search',
           hintStyle: AppTextStyle.withColor(
             AppTextStyle.buttonMedium,
-            isDark? Colors.grey[400]! : Colors.grey[600]!,
+            isDark ? Colors.grey[400]! : Colors.grey[600]!,
           ),
+
           prefixIcon: Icon(
             Icons.search,
-            color: isDark? Colors.grey[400] : Colors.grey[600],
+            color: isDark ? Colors.grey[400] : Colors.grey[600],
           ),
+
+          suffixIcon: Icon(
+            Icons.tune,
+            color: isDark ? Colors.grey[400] : Colors.grey[600],
+          ),
+
           filled: true,
-          fillColor: isDark? Colors.grey[800]! : Colors.grey[100]!,
+
+          fillColor: isDark ? Colors.grey[800]! : Colors.grey[100]!,
+
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(13),
-            borderSide: BorderSide.none
-          )
+            borderSide: BorderSide.none,
+          ),
+
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(13),
+            borderSide: BorderSide(
+              color: isDark ? Colors.grey[400]! : Colors.grey[600]!,
+              width: 1,
+            ),
+          ),
+
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(13),
+            borderSide: BorderSide(
+              color: Theme.of(context).primaryColor,
+              width: 1,
+            ),
+          ),
         ),
       ),
     );
