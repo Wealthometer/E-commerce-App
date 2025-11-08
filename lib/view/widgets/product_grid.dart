dd7bcf3306496901a367c122cd2ef2e9d81ef80c
Wealthometer <wealthhajoh87@gmail.com>
2025-11-08 07:10:37 +0100

    Create product_grid.dart

---
+++ lib/view/widgets/product_grid.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce_app_ui/utils/app_textstyles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
