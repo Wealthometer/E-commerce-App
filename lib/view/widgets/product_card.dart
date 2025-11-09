19714e485c95759a98d39e9c3917646b130b19cc
Wealthometer <wealthhajoh87@gmail.com>
2025-11-09 07:59:35 +0100

    Create product_card.dart

---
+++ lib/view/widgets/product_card.dart
@@ -0,0 +1,123 @@
+import 'package:ecommerce_app_ui/models/product.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+import '../../utils/app_textstyles.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  const ProductCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Container(
+      constraints: BoxConstraints(maxWidth: screenWidth * 0.9),
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: isDark
+                ? Colors.black.withOpacity(0.3)
+                : Colors.grey.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(12),
+                  ),
+                  child: Image.asset(
+                    product.imageUrl,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              Positioned(
+                right: 8,
+                top: 8,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                    color: product.isFavorite
+                        ? Theme.of(context).primaryColor
+                        : isDark
+                        ? Colors.grey[400]
+                        : Colors.grey,
+                  ),
+                ),
+              ),
+
+              if (product.oldPrice != null)
+                Positioned(
+                  left: 8,
+                  top: 8,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Text(
+                      '',
+                      style: AppTextStyle.withColor(
+                        AppTextStyle.withWeight(
+                          AppTextStyle.buttonSmall,
+                          FontWeight.bold,
+                        ),
+                        Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(screenWidth * 0.02),
+            child: Column(
+              children: [
+                Text(
+                  product.name,
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.withWeight(AppTextStyle.h3, FontWeight.bold),
+                    Theme.of(context).textTheme.bodyLarge!.color!,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: screenWidth * 0.01),
+                Text(
+                  product.category,
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.bodyMedium,
+                    Theme.of(context).textTheme.bodyLarge!.color!,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
