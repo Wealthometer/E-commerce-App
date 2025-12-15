24b5b64b37eb5c8da98516c8a1338f3c1b402fc3
Wealthometer <wealthhajoh87@gmail.com>
2025-12-15 16:40:09 +0100

    Update product_grid.dart

--- lib/view/widgets/product_grid.dart
+++ lib/view/widgets/product_grid.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app_ui/models/product.dart';
 import 'package:ecommerce_app_ui/utils/app_textstyles.dart';
+import 'package:ecommerce_app_ui/view/product_details_screen.dart';
 import 'package:ecommerce_app_ui/view/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
