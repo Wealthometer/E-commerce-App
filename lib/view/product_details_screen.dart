d98e96e957cd056d52d980edf89779e080598af0
Wealthometer <wealthhajoh87@gmail.com>
2025-12-16 04:40:20 +0100

    Update product_details_screen.dart

--- lib/view/product_details_screen.dart
+++ lib/view/product_details_screen.dart
@@ -1,11 +1,14 @@
+import 'package:ecommerce_app_ui/models/product.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
-  const ProductDetailsScreen({super.key});
+  final Product product;
+  const ProductDetailsScreen({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
