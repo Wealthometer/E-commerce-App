0e67eddafebc30caf2e103f33f1a39f8cc04b786
Wealthometer <wealthhajoh87@gmail.com>
2025-12-16 08:20:04 +0100

    Update product_details_screen.dart

--- lib/view/product_details_screen.dart
+++ lib/view/product_details_screen.dart
@@ -71,5 +71,6 @@ class ProductDetailsScreen extends StatelessWidget {
 
     final String shopLink = 'https://example.com/products/' + productName;
     final String shareMessage = '$description\n\nShop now @ $shopLink';
+
   }
 }
