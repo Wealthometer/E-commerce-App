000fb04acdcde765f2fb216becd0b17693858403
Wealthometer <wealthhajoh87@gmail.com>
2025-12-17 13:24:21 +0100

    Update product_details_screen.dart

--- lib/view/product_details_screen.dart
+++ lib/view/product_details_screen.dart
@@ -66,6 +66,7 @@ class ProductDetailsScreen extends StatelessWidget {
 
                 //favorite button
                 Positioned(
+                    child: IconButton(
                 )
               ],
             )
