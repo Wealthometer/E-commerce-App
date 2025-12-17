e812771c9b5eb5acd1e1ddda2f7466dccffd2854
Wealthometer <wealthhajoh87@gmail.com>
2025-12-17 13:28:17 +0100

    Update product_details_screen.dart

--- lib/view/product_details_screen.dart
+++ lib/view/product_details_screen.dart
@@ -128,6 +128,7 @@ class ProductDetailsScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
+                    height: screenWidth * 0.01,
             )
           ],
         ),
