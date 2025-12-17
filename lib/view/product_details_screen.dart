c546c490cfe73016a8d662dd3ed1486c06f262d2
Wealthometer <wealthhajoh87@gmail.com>
2025-12-17 13:25:57 +0100

    Update product_details_screen.dart

--- lib/view/product_details_screen.dart
+++ lib/view/product_details_screen.dart
@@ -84,6 +84,7 @@ class ProductDetailsScreen extends StatelessWidget {
             //product details
             Padding(
                 padding: EdgeInsets.all(screenWidth * 0.04),
+              child: Column(
             )
           ],
         ),
