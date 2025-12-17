775fc61ad52a2a3f39764ffed549ba573a2c7cd9
Wealthometer <wealthhajoh87@gmail.com>
2025-12-17 13:38:57 +0100

    Update product_details_screen.dart

--- lib/view/product_details_screen.dart
+++ lib/view/product_details_screen.dart
@@ -205,6 +205,7 @@ class ProductDetailsScreen extends StatelessWidget {
                           ),
                           backgroundColor: Theme.of(context).primaryColor,
                           side: BorderSide(
+                              color: isDark ? Colors.white70 : Colors.black87
     );
   }
 
