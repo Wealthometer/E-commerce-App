bafe72e5c370aaef4d3e5364056d22a49b714a1a
Wealthometer <wealthhajoh87@gmail.com>
2026-01-15 13:31:53 +0100

    Update custom_bottom_navbar.dart

--- lib/view/widgets/custom_bottom_navbar.dart
+++ lib/view/widgets/custom_bottom_navbar.dart
@@ -1,5 +1,5 @@
 import 'package:ecommerce_app_ui/controllers/navigation_controller.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
